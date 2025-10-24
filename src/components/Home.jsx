791afdf57c1a53d48aa03098a21d68226ffc1828
mjh-shikder <mjhsikder@gmail.com>
2025-10-24 18:38:25 +0600

    Register Hero added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h1>Home</h1>
+            <h1 className='text-3xl text-center font-semibold text-green-700 my-24'> Home Page</h1>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import Root from './layout/Root.jsx';
 import Home from './components/Home.jsx';
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,14 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home,
+      },
+      {
+        path: 'register',
+        Component: Register,
+      },
+      {
+        path: 'login',
+        Component: Login,
       }
     ]
   },
