0c62316f14412329bcc305a9bca57a7168000c24
mjh-shikder <mjhsikder@gmail.com>
2025-10-24 19:06:10 +0600

    firebase installed

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 
 const Register = () => {
+
+    const handleRegister = (e) => {
+    
+        e.preventDefault()
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+
+        console.log(email, password);
+
+       
+        
+}
+
+
+
     return (
                
        <div className="hero bg-base-200 min-h-screen">
@@ -14,12 +31,12 @@ const Register = () => {
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
         <div className="card-body">
-        <form>
+        <form onSubmit={handleRegister}>
         <fieldset className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" className="input" placeholder="Email" name='email' />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" className="input" placeholder="Password" name='password' />
           <div><a className="link link-hover">Forgot password?</a></div>
           <button className="btn btn-neutral mt-4">Register</button>
           </fieldset>
